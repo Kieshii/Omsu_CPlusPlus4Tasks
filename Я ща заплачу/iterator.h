b75c83c4bd38964698d9ade34971b83ad7f31552
Kieshii <Kieshii@DESKTOP-NS8DE4S>
2022-05-30 02:05:31 +0600

    Добавьте файлы проекта.

---
+++ Я ща заплачу/iterator.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#include "hashtable.h"
+
+class Iterator {
+private:
+    int index;
+    int length;
+    int buffer;
+    Node* elem;
+    Node* arr;
+public:
+    Iterator(HashTable&);
+    void start();
+    void next();
+    bool finish();
+    int getValue();
+};
